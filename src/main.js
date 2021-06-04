f803f7967b853e8a59bf3fed4cff61c92fac94e4
Vasyl Petrus <petrusvasyl.j@gmail.com>
2021-06-04 18:08:57 +0300

    add axios

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import zircle from 'zircle'
 import 'zircle/dist/zircle.css'
+import axios from 'axios'
+
+Vue.use(axios)
 Vue.use(zircle)
 Vue.config.productionTip = false
 
--- src/views/device.vue
+++ src/views/device.vue
@@ -1,63 +1,40 @@
 <template>
   <z-view>
     {{ category }}
-    <button v-on:click="setState">{{ state ? "on" : "off" }}</button>
+    <button v-on:click="setState">{{ state ? 'on' : 'off' }}</button>
   </z-view>
 </template>
 <script>
 /*eslint-disable*/
+import axios from 'axios';
 // just $route.params.category and $route.params.qty
 export default {
-  data: function () {
+  data: function() {
     return {
-      state: "",
+      state: '',
     };
   },
 
   methods: {
-    getState: function () {
+    getState: function() {
       var app = this;
       fetch(`http://localhost:3000/devices/edit/60ad139e2e00712fc4d60be7`)
         .then((response) => response.json())
         .then((data) => (app.state = data.state))
-        .catch(function (error) {
+        .catch(function(error) {
           console.log(error.message);
         });
     },
-    setState: function () {
-      let formData = new FormData();
-      formData.append("id", "60ad139e2e00712fc4d60be7");
-      formData.append("State", !this.state);
-      const requestOptions = {
-        method: "POST", // *GET, POST, PUT, DELETE, etc.
-        mode: "cors", // no-cors, *cors, same-origin
-        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: "same-origin",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          id: "60ad139e2e00712fc4d60be7",
-          State: !this.state,
-        }),
+    setState: function() {
+      const body = {
+        id: '60ad139e2e00712fc4d60be7',
+        State: !this.state,
       };
-      
-      fetch(`http://localhost:3000/devices/edit`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          id: "60ad139e2e00712fc4d60be7",
-          State: !this.state,
-        }),
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          console.log(data);
-        })
-        .catch(function (error) {
-          console.log("err", error.message);
+      axios
+        .post('http://localhost:3000/devices/edit', body)
+        .then((response) => console.log(response))
+        .catch((error) => {
+          console.error('There was an error!', error);
         });
     },
   },
@@ -80,4 +57,3 @@ export default {
   },
 };
 </script>
-
